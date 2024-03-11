62251b90e9db264f007fbb13e0a5bb5e142e399d
HardCodeMatter <dklymovets1@gmail.com>
2024-03-11 13:42:15 +0200

    Added alembic in project, created revisions for auth models.

--- src/auth/models.py
+++ src/auth/models.py
@@ -7,17 +7,17 @@ from database.database import Base
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
-    __tablename__ = 'uesr'
+    __tablename__ = 'user'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(30), unique=True)
     email: Mapped[str] = mapped_column(String(150), unique=True)
     hashed_password: Mapped[str]
-    created_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utf', now())"))
+    created_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
     updated_at: Mapped[datetime] = mapped_column(
-        server_default=text("TIMEZONE('utf', now())"),
+        server_default=text("TIMEZONE('utc', now())"),
         onupdate=datetime.utcnow,
     )
     is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
-    is_superuser: Mapped[bool] = mapped_column(default=True, nullable=False)
-    is_verified: Mapped[bool] = mapped_column(default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
